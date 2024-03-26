3b8974c8e5809584ddf2505c1c23bda526c41a65
b21ricni <b21ricni@student.his.se>
2024-03-26 15:50:26 +0100

    Added Routing
    +Routing
    +Home page
    +Cart
    +Products
    -Example code

--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductsComponent } from './products/products.component';
 
 export const routes: Routes = [
-  {'path': 'cart', 'component': CartComponent},
+  {path: '', component: HomepageComponent},
+  {path: 'Cart', component: CartComponent},
+  {path: 'Product', component: ProductsComponent},
 ];
