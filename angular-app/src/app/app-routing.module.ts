9b6eee1ac87825661dddbe38601999c248c51579
b21ricni <b21ricni@student.his.se>
2024-04-02 16:09:01 +0200

    Removed "Landingpage"
    -Removed landingoage from app routing and components

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'ecart', loadChildren: () => import('./ecart/ecart.module').then(m => m.EcartModule) }, { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }, { path: 'landingpage', loadChildren: () => import('./landingpage/landingpage.module').then(m => m.LandingpageModule) }];
+const routes: Routes = [
+  { path: 'ecart', loadChildren: () => import('./ecart/ecart.module').then(m => m.EcartModule) }, 
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
