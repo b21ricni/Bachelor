1a409299303773102c7b3ac7a94b684642519eae
b21ricni <b21ricni@student.his.se>
2024-04-02 16:23:29 +0200

    Code cleaning

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -2,8 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'ecart', loadChildren: () => import('./ecart/ecart.module').then(m => m.EcartModule) }, 
-  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+  { 
+    path: 'ecart', 
+    loadChildren: () => import('./ecart/ecart.module').then(m => m.EcartModule) 
+  }, 
+  {  
+    path: 'products', 
+    loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) 
+  },
+  { 
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
   ];
 
 @NgModule({
--- angular-app/src/app/products/products-routing.module.ts
+++ angular-app/src/app/products/products-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './products.component';
 
-const routes: Routes = [{ path: '', component: ProductsComponent }];
+const routes: Routes = [
+  { 
+    path: '', 
+    component: ProductsComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
