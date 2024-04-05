603f7bae318b07aa74553cd19ebd3ab58fe1b4b0
b21ricni <b21ricni@student.his.se>
2024-04-05 17:26:51 +0200

    Fixed Lazy routing error

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 //Without Lazy Loading
 //import Cart from "./Pages/Cart";
@@ -9,22 +9,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import Layout from "./Pages/Header";
 
 //With Lazy Loading
-const Cart = React.lazy(() => import("./Pages/Cart"))
-const Products = React.lazy(() => import("./Pages/Products"))
-const Home = React.lazy(() => import("./Pages/Home"))
-const Header = React.lazy(() => import("./Pages/Header"))
+const Cart = React.lazy(() => import("./Pages/Cart"));
+const Products = React.lazy(() => import("./Pages/Products"));
+const Home = React.lazy(() => import("./Pages/Home"));
+const Header = React.lazy(() => import("./Pages/Header"));
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Header />}>
-          <Route index element={<Home />} />
-          <Route path="Cart" element={<Cart />} />
-          <Route path="Products" element={<Products />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Home />} />
+            <Route path="Cart" element={<Cart />} />
+            <Route path="Products" element={<Products />} />
+          </Route>
+        </Routes>
+      </React.Suspense>
+    </Router>
   );
 }
 
