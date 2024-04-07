6284429793ba202d88bf012c7781d4ae1ecac87c
b21ricni <b21ricni@student.his.se>
2024-04-07 19:35:31 +0200

    Simple array

--- react-app/src/Pages/Cart.js
+++ react-app/src/Pages/Cart.js
@@ -1,5 +1,15 @@
 const Cart = () => {
-  return <p>Cart</p>;
+  return (
+    <>
+      <div className="">
+        {Array(5).fill().map(index => {
+          return <div key={index}>
+              <p>Hej testing {index}</p>
+            </div>
+        })}
+      </div>
+    </> 
+  )
 }
 
 export default Cart;
