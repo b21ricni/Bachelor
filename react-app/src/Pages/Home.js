3eed3fc62310df364b6a8495221d04be93a24d33
b21ricni <b21ricni@student.his.se>
2024-04-09 14:28:25 +0200

    Update Home.js
    +useState for search word
    +handle submit
    +set search word function
    +label

--- react-app/src/Pages/Home.js
+++ react-app/src/Pages/Home.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 function Home(){
+  const [searchWord, setSearchWord] = useState("")
+
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log("btn pressed")
+    console.log("Form submitted");
+    console.log("Search word:", searchWord);
+  }
+
+  const handleInputChange = (e) => {
+    setSearchWord(e.target.value)
+  }
+
   return (
     <>
       <h1>Home</h1>
       <div>
-        <form>
-          <input className="input-search-prduct" />
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="searchInput">Search for product</label>
+          <input className="input-search-prduct" onChange={handleInputChange} />
           <button className="btn-search-product" type="submit">Search</button>
         </form>
       </div>
